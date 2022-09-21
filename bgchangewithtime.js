99e819877616c2ac58586d8751c4bf47a6e92960
Ramkrishn <ram706860@gmail.com>
2022-09-21 13:27:15 +0530

    colorChage

--- bgchangewithtime.js
+++ bgchangewithtime.js
@@ -7,7 +7,7 @@ function setBgToBlack() {
     r.style.setProperty('--bg-color', '#000');
 }
 function setBgToWhite() {
-    r.style.setProperty('--bg-color', '#fff');   
+    r.style.setProperty('--bg-color', '#0ff7');   
 }
 
 function changeBg() {
